be1da4a5dfda2ba29ae88778b48ef022026deb20
ibdch <ibdch@10.18.148.244>
2016-06-03 09:59:52 -0600

    5/12/16 - Initial Commitment

--- src/citbyui/cit260/SpaceExploration/view/MainMenuView.java
+++ src/citbyui/cit260/SpaceExploration/view/MainMenuView.java
@@ -9,10 +9,44 @@ package citbyui.cit260.SpaceExploration.view;
  *
  * @author ibdch
  */
-class MainMenuView {
-
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+public class MainMenuView {
+    
+    private String menu;
+    
+    public void displayMainMenuView() {
+        boolean done = false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("E")) //user wants to exit
+                return; // exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+        } while (!done);
     }
+    
+    public MainMenuView() {
+        this.menu = "\n"
+                  + "\n--------------------------------------"
+                  + "\n| Main Menu                           "
+                  + "\n--------------------------------------"
+                  + "\nN - Start New Game"
+                  + "\nL - Load Saved Game"
+                  + "\nH - Open Help Menu"
+                  + "\nE - Exit"
+                  + "\n--------------------------------------";
+    }
+
+    private String getMenuOption() {
+        System.out.println("\n*** getMenuOption() function called ***");
+        return "N";
+    }
+
+    private boolean doAction(String menuOption) {
+        System.out.println("\n*** doAction() function called ***");
+        return true;
+    }
+    
     
 }
