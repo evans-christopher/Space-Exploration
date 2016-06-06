89e0f1547024860ffde5298697a9318c376d7111
Erik <Erik@Erik-PC>
2016-06-06 09:57:20 -0600

    6-6-2016 Broken help Menu

--- src/citbyui/cit260/SpaceExploration/view/HelpMenuView.java
+++ src/citbyui/cit260/SpaceExploration/view/HelpMenuView.java
@@ -5,6 +5,8 @@
  */
 package citbyui.cit260.SpaceExploration.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Erik
@@ -13,22 +15,23 @@ public class HelpMenuView {
     
    
     private String menu;
+    private final String menuHelp;
     
     public void displayHelpMenuView() {
         boolean done = false; // set flag to not done
         do {
             // prompt for and get players name
-            String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals("E")) //user wants to exit
+            String helpMenuOption = this.getHelpMenuOption();
+            if (helpMenuOption.toUpperCase().equals("E")) //user wants to exit
                 return; // exit the game
             
             // do the requested action and display the next view
-            done = this.doAction(menuOption);
+            done = this.doAction(helpMenuOption);
         } while (!done);
     }
     
     public HelpMenuView() {
-        this.menu = "\n"
+        this.menuHelp = "\n"
                   + "\n--------------------------------------"
                   + "\n| Help Menu                          |"
                   + "\n--------------------------------------"
@@ -39,31 +42,47 @@ public class HelpMenuView {
                   + "\nE - Exit"
                   + "\n--------------------------------------";
     }
-
-    private String getMenuOption() {
-        System.out.println("\n*** getMenuOption() function called ***");
-        return "N";
+  private String getHelpMenuOption() {
+        /*System.out.println("\n*** getHelpMenuOption() function called ***");
+        return "N";*/
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; //value to  be returned
+        boolean valid = false; //initialize to not valid
+        
+        while (!valid) { //loop while an invalid value is entered
+            System.out.println("\n" + this.menuHelp);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break; //end the loop
+        }
+        
+        return value; //return vaule entered
     }
 
+ 
 public boolean doAction(String choice){
-System.out.println("\n*** doAction() function called ***");
-        return true;
-    }
     
    choice = choice.toUpperCase(); //convert choice to upper case
    switch (choice) {
    //return true;
    case "W": //Objectives description
-   this.objectivehelp();
+   this.objectHelp();
    break;
    case "H": // Moving and Navigating
-   this.navigatehelp();
+   this.navigateHelp();
    break;
    case "G": //Obtaining and gathering resources
-   this.gatherhelp();
+   this.gatherHelp();
    break;
    case "R": // Help for repairing ship
-   this.repairship();
+   this.repairShip();
    break;
    default:
    System.out.println("\n*** Invalid selection *** Please try again");
@@ -74,4 +93,24 @@ System.out.println("\n*** doAction() function called ***");
     return false;
     
 }
+
+    private void objectHelp() {
+      System.out.println("Objective Help function called");
+    }
+
+    private void navigateHelp() {
+      System.out.println("Objective Navigate Help function called");
+    }
+
+    private void gatherHelp() {
+      System.out.println("Objective Gather Help function called");
+    }
+
+    private void repairShip() {
+      System.out.println("Objective Repair Ship function called");
+      
+      
+    }
+    }
+    
     
